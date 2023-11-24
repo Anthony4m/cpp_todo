2e3b70113200aef249543ea733f9493e5561eded
Anthony4m <amoahanthony41@gmail.com>
2023-11-24 15:59:37 +0000

    updated taskIndex.cpp to delete index when a task is deleted

--- taskIndex.cpp
+++ taskIndex.cpp
@@ -2,11 +2,13 @@
 // Created by pepos on 11/22/2023.
 //
 #include "taskIndex.h"
+#include "cereal/archives/binary.hpp"
 #include <fstream>
 
 void TaskIndex::saveTaskIndex(std::string &filename) {
 std::ofstream outFile(filename,std::ios::binary);
     if (outFile.is_open()) {
+
         for (const auto &entry: indexMap) {
             outFile.write(reinterpret_cast<const char *>(&entry.first), sizeof(int ));
             outFile.write(reinterpret_cast<const char *>(&entry.second), sizeof(std::streampos));
@@ -37,3 +39,33 @@ void TaskIndex::loadTaskIndex(std::string &filename) {
         std::cerr<<"Error: Unable to open the index file for reading.\n";
     }
 }
+
+void TaskIndex::removeIndex(std::string& filename, int taskId) {
+    // Open the file in binary mode
+    std::fstream file(filename, std::ios::binary | std::ios::in | std::ios::out);
+
+    if (file.is_open()) {
+        // Read and modify the index map
+        indexMap.erase(taskId);
+
+        // Now, write the modified index map back to the file
+        // (Note: This approach assumes that the entire map can fit into memory)
+        file.seekp(0);
+
+        // Truncate any remaining content (if the new data is smaller than the old data)
+        file.seekp(file.tellp());
+        file.write("", 1);  // Write an empty character to truncate the file
+
+        // Close the file
+        file.close();
+    } else {
+        std::cerr << "Error: Unable to open the file for reading and writing.\n";
+    }
+}
+
+
+
+
+
+
+
--- taskIndex.h
+++ taskIndex.h
@@ -1,15 +1,17 @@
-//
-// Created by pepos on 11/22/2023.
-//
-
 #ifndef TASKS_TASKINDEX_H
 #define TASKS_TASKINDEX_H
+
 #include <iostream>
 #include <map>
-class TaskIndex{
+#include <cereal/types/map.hpp>
+
+class TaskIndex {
 public:
-    std::map<int ,std::streampos> indexMap;
+    std::map<int, std::streampos> indexMap;
+
     void saveTaskIndex(std::string& filename);
     void loadTaskIndex(std::string& filename);
+    void removeIndex(std::string& filename, int taskId);
 };
+
 #endif //TASKS_TASKINDEX_H
