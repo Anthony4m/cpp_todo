07f4e42236594f9cf93c30136111e970b83a6ebc
Anthony4m <amoahanthony41@gmail.com>
2023-11-27 22:18:04 +0000

    Cleaned up the main to inplement a repl

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include <cereal/archives/binary.hpp>
 #include <fstream>
-#include "task.h"  // Include your Task class definition
+#include <iostream>
+#include <string>
+#include "task.h"
+#include "repl.h"
 
 int main() {
     // Create an instance of the Task class
     tasks::Task myTask;
-//    myTask.createTask("Church Task", "This is a sample task", priority::HIGH, "2023-12-31");
-//    myTask.createTask("School Task", "This is a sample task", priority::HIGH, "2023-12-31");
-//    myTask.createTask("Work Task", "This is a sample task", priority::HIGH, "2023-12-31");
-//    myTask.createTask("Home Task", "This is a sample task", priority::HIGH, "2023-12-31");
-    myTask.getTask(2);
-    myTask.updateTaskStatus(2,status::IN_PROGRESS);
-    myTask.updateTaskPriority(2,priority::LOW);
-    myTask.getTask(2);
-//    myTask.deleteTask(2);
+    std::cout << "Task Manager REPL. Type 'help' for a list of commands.\n";
+    while (true) {
+        std::string command;
+        std::cout << "> ";
+        std::getline(std::cin, command);
+
+        // Process the command
+        processCommand(command, myTask);
+    }
     return 0;
 }
